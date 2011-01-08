e5ae199bb657a4cf926cf93b29a7c1ee7a02b6b0
yinonavraham <yinonavraham@1d8acc1c-d249-9207-2277-153f288343ce>
2011-01-08 21:23:09 +0000

    Add a main method with command arguments handling, as the entry point for the applications

---
+++ trunk/src/ty/tech/prioritizedJobRep/CommandOption.java
@@ -0,0 +1,21 @@
+package ty.tech.prioritizedJobRep;
+
+
+public enum CommandOption
+{
+	StartServer,
+	StopServer,
+	RegisterServer,
+	StartDispatcher,
+	StopDispatcher,
+	StartClient;
+	
+	public static CommandOption valueOfIgnoreCase(String name)
+	{
+		for (CommandOption cmdOp : values())
+		{
+			if (cmdOp.name().equalsIgnoreCase(name)) return cmdOp;
+		}
+		return null;
+	}
+}
