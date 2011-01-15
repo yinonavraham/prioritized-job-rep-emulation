0a3c12363d28829e509e82eeaeea6e6128e81b24
yinonavraham <yinonavraham@1d8acc1c-d249-9207-2277-153f288343ce>
2011-01-15 10:32:44 +0000

    Rename ConnectionFactory to ProxyFactory

---
+++ trunk/src/ty/tech/prioritizedJobRep/api/ProxyFactory.java
@@ -0,0 +1,20 @@
+package ty.tech.prioritizedJobRep.api;
+
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import ty.tech.prioritizedJobRep.common.Entities;
+import ty.tech.prioritizedJobRep.server.Server;
+
+
+public class ProxyFactory
+{
+	public static Server createServerProxy(String host, int port) throws AccessException, RemoteException, NotBoundException
+	{
+		Registry registry = LocateRegistry.getRegistry(host, port);
+		return (Server)registry.lookup(Entities.SERVER);
+	}
+}
