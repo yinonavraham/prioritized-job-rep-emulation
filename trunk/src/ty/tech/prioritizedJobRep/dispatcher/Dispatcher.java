c0eb91de57a17f94fe80c7753f1ac6bea9004d30
tal.kellner <tal.kellner@1d8acc1c-d249-9207-2277-153f288343ce>
2011-01-29 12:34:21 +0000

    Continue dispatcher implementation

--- trunk/src/ty/tech/prioritizedJobRep/dispatcher/Dispatcher.java
+++ trunk/src/ty/tech/prioritizedJobRep/dispatcher/Dispatcher.java
@@ -3,9 +3,12 @@ package ty.tech.prioritizedJobRep.dispatcher;
 import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 import ty.tech.prioritizedJobRep.common.EndPoint;
 import ty.tech.prioritizedJobRep.common.Job;
+import ty.tech.prioritizedJobRep.common.JobResult;
+import ty.tech.prioritizedJobRep.common.ServerStatistics;
 
 public interface Dispatcher extends Remote
 {
@@ -31,6 +34,34 @@ public interface Dispatcher extends Remote
 	 * Adds a new job to the dispatcher
 	 * @param job
 	 */
-	void addJob(Job job) throws RemoteException;	
+	void addJob(Job job) throws RemoteException;
+	
+	/**
+	 * Gets a job's result from a server and keeps it
+	 * @param result
+	 */
+	void keepJobResults(JobResult result) throws RemoteException;
+	
+	/**
+	 * Returns a map with the result of all jobs in the iteration
+	 */
+	ArrayList<JobResult> getJobsResults() throws RemoteException;
+	
+	/**
+	 * Get a list of all servers statistics in an iteration
+	 */
+	ArrayList<ServerStatistics> getServersStatistics() throws RemoteException;
+	
+	/**
+	 * Resets all servers from their current iteration's jobs processing
+	 * @throws RemoteException
+	 */
+	void resetAllServers() throws RemoteException;
+	
+	/**
+	 * Stops all servers
+	 * @throws RemoteException
+	 */
+	void stopAllServers() throws RemoteException;
 
 }
--- trunk/src/ty/tech/prioritizedJobRep/dispatcher/JobSenderThread.java
+++ trunk/src/ty/tech/prioritizedJobRep/dispatcher/JobSenderThread.java
@@ -1,17 +1,21 @@
 package ty.tech.prioritizedJobRep.dispatcher;
 
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Random;
 
 import ty.tech.prioritizedJobRep.common.Job;
 import ty.tech.prioritizedJobRep.common.Priority;
+import ty.tech.prioritizedJobRep.logging.Logger;
 import ty.tech.prioritizedJobRep.server.Server;
 
 public class JobSenderThread extends Thread 
 {
+	private boolean _isDispatcherRunning;
+	
 	public JobSenderThread()
 	{
-		
+		_isDispatcherRunning = true;
 	}
 	
 	public void run() 
@@ -19,39 +23,53 @@ public class JobSenderThread extends Thread
 		Job hpJob = null;
 		Job lpJob = null;
 		
-		while(true)
+		while(_isDispatcherRunning)
 		{
 			Job job = null;
 			job = DispatcherImpl.getIncomingJobsQueue().pop();
-			if (null != hpJob)
+			if (null != job)
 			{
-				//copy the job
-				hpJob = job;
-				lpJob = job;
-				
-				//set matching priorities
-				hpJob.setPriority(Priority.High);
-				lpJob.setPriority(Priority.Low);
+				//copy the job and set matching priorities
+				hpJob = job.clone(Priority.High);
+				lpJob = job.clone(Priority.Low);
 				
 				// get all available servers
 				ArrayList<Server> servers = DispatcherImpl.getActiveServers();
 				
 				// get random 2 servers out of these
 				int idx1 = -1, idx2 = -1;
-				boolean con1 = false, con2 = false;
 			    Random randomGenerator = new Random();
-			    while (idx1 == idx2 && con1 && con2)
+			    
+			    // get first server index
+			    while(idx1 == -1)
 			    {
-			    	idx1 = randomGenerator.nextInt(servers.size());
-			    	con1 = servers.get(idx1).ping();
-			    	if (con1)
+			    	try
 			    	{
-				    	idx2 = randomGenerator.nextInt(servers.size());
-				    	con2 = servers.get(idx2).ping();			    		
+					    idx1 = randomGenerator.nextInt(servers.size());
+					    servers.get(idx1).ping(); // will throw if there is no answer
 			    	}
-
+			    	catch(RemoteException e)
+			    	{
+			    		System.err.println("Cannot ping registered server");
+			    		Logger.getLocation(this.getClass()).throwing("JobServerThread.run()", e);
+			    	}			    	
 			    }
 			    
+			    // get the second server index until it is different than the first one
+			    while(idx2 == -1 || idx1 == idx2)
+			    {
+			    	try
+			    	{
+					    idx2 = randomGenerator.nextInt(servers.size());
+					    servers.get(idx2).ping(); // will throw if there is no answer
+			    	}
+			    	catch(RemoteException e)
+			    	{
+			    		System.err.println("Cannot ping registered server");
+			    		Logger.getLocation(this.getClass()).throwing("JobServerThread.run()", e);
+			    	}			    	
+			    }			    
+
 			    // send the jobs and move the original one to the inProcessQueue
 			    try 
 			    {
@@ -61,9 +79,9 @@ public class JobSenderThread extends Thread
 			    }
 			    catch(Exception e) 
 				{
+			    	e.printStackTrace();
 					System.err.println("Error occured in JobSenderThread run: " + e.getMessage());
 				} 
-
 			}
 			try 
 			{
@@ -71,8 +89,15 @@ public class JobSenderThread extends Thread
 			}
 			catch (InterruptedException e) 
 			{
-				System.err.println("Error occured in JobsGeneratorThread sleep: " + e.getMessage());
+				System.err.println("Dispatcher.JobSenderThread stopped");
 			}
 		}
+		System.out.println("Dispatcher.JobSenderThread stopped");
+		Logger.getLocation(this.getClass()).debug("Dispatcher.JobSenderThread stopped");
+	}
+	
+	public void setIsDispatcherRunning(boolean status)
+	{
+		_isDispatcherRunning = false;
 	}
 }
