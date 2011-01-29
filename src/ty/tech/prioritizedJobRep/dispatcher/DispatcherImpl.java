7e68e01a309436e64cd91d8bb5beb49755f61317
tal.kellner <tal.kellner@1d8acc1c-d249-9207-2277-153f288343ce>
2011-01-29 12:34:21 +0000

    Continue dispatcher implementation

--- src/ty/tech/prioritizedJobRep/dispatcher/DispatcherImpl.java
+++ src/ty/tech/prioritizedJobRep/dispatcher/DispatcherImpl.java
@@ -4,14 +4,14 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 import java.util.ArrayList;
 
+import ty.tech.prioritizedJobRep.api.ProxyFactory;
 import ty.tech.prioritizedJobRep.common.EndPoint;
-import ty.tech.prioritizedJobRep.common.Entities;
 import ty.tech.prioritizedJobRep.common.FIFOQueue;
 import ty.tech.prioritizedJobRep.common.Job;
+import ty.tech.prioritizedJobRep.common.JobResult;
+import ty.tech.prioritizedJobRep.common.ServerStatistics;
 import ty.tech.prioritizedJobRep.logging.Location;
 import ty.tech.prioritizedJobRep.logging.Logger;
 import ty.tech.prioritizedJobRep.server.Server;
@@ -20,23 +20,19 @@ public class DispatcherImpl implements Dispatcher
 {
 	private static ArrayList<Server> _activeServers;
 	private static ArrayList<Job> _inProgressJobsQueue;
+	private static ArrayList<JobResult> _jobsResults;
 	private Location _location = Logger.getLocation(this.getClass());
 	private boolean _finished;
 	private static FIFOQueue _incomingJobsQueue;
 	
-	JobSenderThread _jobSenderThread = null;
-	
-	//1. Incoming jobs queue
-	//2. Jobs in progress queue - fifo queue
-	//3. Jobs results queue
-	//4. Jobs replicator & sender
-
+	private JobSenderThread _jobSenderThread = null;
 	
 	public DispatcherImpl(int port) throws SocketException, UnknownHostException
 	{
 		_activeServers = new ArrayList<Server>();
 		_finished = false; 
 		_incomingJobsQueue = new FIFOQueue("Dispatcher's Incoming Jobs Queue");
+		_inProgressJobsQueue = new ArrayList<Job>();
 	}
 	
 	public void start()
@@ -62,8 +58,8 @@ public class DispatcherImpl implements Dispatcher
 		_finished = true;
 		//stop all running threads
 		Thread.currentThread().interrupt();
-		_jobSenderThread.interrupt();
-		_location.exiting("stop()");
+		_jobSenderThread.setIsDispatcherRunning(false);
+		_location.exiting("stop()");	
 	}
 	
 	@Override
@@ -72,15 +68,18 @@ public class DispatcherImpl implements Dispatcher
 		_location.entering("registerServer(EndPoint endPoint)", endPoint);
 		
 		// locate server's registry and keep it in active server list
-        Registry registry = LocateRegistry.getRegistry(endPoint.getHostAddress());
-        Server server = (Server) registry.lookup(Entities.SERVER);
+        Server server = ProxyFactory.createServerProxy(endPoint);
         _activeServers.add(server);
+        
+        String msg = "Server " + endPoint.getHostName()+ ":" + endPoint.getPort()+ " was registered";
+        System.out.println(msg);
+       _location.debug(msg);
 
 		_location.exiting("registerServer(EndPoint endPoint)");
 	}
 	
 	@Override
-	public void removeServer(EndPoint endPoint)
+	public synchronized void removeServer(EndPoint endPoint)
 	{
 		_location.entering("removeServer(EndPoint endPoint)", endPoint);
 		
@@ -96,13 +95,63 @@ public class DispatcherImpl implements Dispatcher
 	}	
 	
 	@Override
-	public void addJob(Job job)
+	public synchronized void addJob(Job job)
 	{
+		job.getStatistics().setStartTime();
 		_incomingJobsQueue.put(job);
 		System.out.println("Dispatcher got job " + job.getID());
 	}
 	
-	public static FIFOQueue getIncomingJobsQueue()
+	@Override
+	public synchronized void keepJobResults(JobResult result)
+	{
+		result.setTotalTimeinSys(System.currentTimeMillis() - result.getStartTime());
+		_jobsResults.add(result);
+	}
+	
+	@Override
+	public ArrayList<JobResult> getJobsResults()
+	{
+		return _jobsResults;
+	}
+	
+	@Override
+	public ArrayList<ServerStatistics> getServersStatistics() throws RemoteException
+	{
+		ArrayList<ServerStatistics> serversStatistics = new ArrayList<ServerStatistics>();
+		
+		for (int i = 0; i < getActiveServers().size(); ++i)
+		{
+			ServerStatistics serverStatistics = getActiveServers().get(i).getStatistics();
+			serversStatistics.add(serverStatistics);
+		}
+		
+		return serversStatistics;
+	}
+	
+	@Override
+	public void resetAllServers() throws RemoteException
+	{
+		for (int i = 0; i < getActiveServers().size() ; ++i)
+		{
+			Server server = getActiveServers().get(i);
+			server.reset();
+	        String msg = "Server" + server.getEndPoint().getHostName()+ ":" + server.getEndPoint().getPort()+ " was reset";
+	        System.out.println(msg);
+	       _location.debug(msg);			
+		}
+	}
+	
+	@Override
+	public void stopAllServers() throws RemoteException
+	{
+		for (int i = 0; i < getActiveServers().size() ; ++i)
+		{
+			getActiveServers().get(i).stop();
+		}
+	}	
+	
+	public synchronized static FIFOQueue getIncomingJobsQueue()
 	{
 		return _incomingJobsQueue;
 	}
